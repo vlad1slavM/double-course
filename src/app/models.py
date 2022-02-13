677f2bfe35601c097406623b498981248260154f
vlad1slavM <vladislav3982@gmail.com>
2022-02-13 17:37:04 +0500

    change models

--- src/app/models.py
+++ src/app/models.py
@@ -1,6 +1,5 @@
 from app.internal.models.admin_user import AdminUser
 from django.db import models
-from django.core.validators import RegexValidator
 
 
 class User(models.Model):
@@ -8,8 +7,7 @@ class User(models.Model):
     username = models.CharField(max_length=32, null=False)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True, blank=True)
+    phoneNumber = models.CharField(max_length=16, unique=True, blank=True)
 
     def __str__(self):
         return f'@{self.username}' if self.username is not None else f'{self.tg_id}'
